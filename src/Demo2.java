11a9ca124f666f88c437e04567fdc9177610d384
lagon <xhtons@outlook.com>
2026-08-22 19:45:25 +0800

    docs/feat: 保存最近的源码练习与路线图进度

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Demo2 {
+    public static void main(String[] args) {
+        float f1 = 3.14789563F;
+        System.out.println(f1);
+
+        double d1 = 3.1415926535;
+        System.out.println(d1);
+
+        Integer i1 = new Integer(1);
+        System.out.println(i1);
+
+
+        Integer.parseInt("200");
+
+        //
+        String s3 = String.valueOf(100);
+        System.out.println(s3);
+        String s4 = Integer.toString(200);
+
+      Double d22 =   Double.parseDouble("3.14");
+      System.out.println(d22);
+    }
 }
--- src/Demo3.java
+++ src/Demo3.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class Demo3 {
+
+    public static void main(String[] args) {
+        // int a=10;
+        // double b=a;
+        //
+        // System.out.println(b);
+        //
+        // double c=20.123;  // 大转换小，必须显式，而且
+        // int d = (int)c;
+        // System.out.println(d);
+
+
+        // 字符串转数字
+        int ac = Integer.parseInt("456");
+        System.out.println("ac ="+ ac);
+
+        int d1 = (int) Double.parseDouble("3.45");
+        System.out.println("d1 ="+ d1);
+
+        // 数字转字符串
+        String s1 = String.valueOf(200);
+        System.out.println("s1 ="+ s1);
+
+        Stri
+        ng s2 = Integer.toString(456);
+        System.out.println("s2 ="+ s2);
+
+        int n1 = 11;
+        Integer n2 = 33;
+
+
+
+
+
+
+    }
 }
