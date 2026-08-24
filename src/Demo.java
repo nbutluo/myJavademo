33531622d3f3c7f4d51599ca6f08c2bf3e277786
lagon <xhtons@outlook.com>
2026-08-24 11:01:41 +0800

    feat: 更新 Demo 与 Demo4 练习代码

--- src/Demo.java
+++ src/Demo.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Demo {
+
+    public static void main(String[] args) {
+        String text = " Java is Awesome! ";
+
+        String cleanText =  text.trim();
+        System.out.println(cleanText);
+
+        System.out.println(cleanText.substring(0,4));
+
+        // 替换
+        System.out.println(cleanText.replace("Awesome", "Great"));
+        // 拆分
+
+        String[] words = cleanText.split(" ");
+        System.out.println(words[0]);
+    }
 }
--- src/Demo4.java
+++ src/Demo4.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Demo4 {
+    public static  void main(String[] args){
+
+        String str1 = "abc";
+        String str2 = "abc";
+
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str2));
+
+        String ss =  "hello".substring(1,4);
+        System.out.println(ss);
+
+
+        System.out.println("----------------------");
+
+        String a = new String("abc");
+        String b = new String("abc");
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+    }
 }
